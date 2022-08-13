7a9a77b80609cbccf54783a1dcfcea7eb46e5aa3
brittonstoffregen <britton@wadeh.com>
2022-08-12 22:48:49 -0500

    Starting tutorial work page + Starting scratch services page and scratch work page.

---
+++ creative-agency-website-scratch/js/script.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector('.toggle-btn')
+const navigation = document.querySelector('.navigation')
+
+toggleBtn.addEventListener('click', () => {
+  toggleBtn.classList.toggle('active')
+})
+
+toggleBtn.addEventListener('click', () => {
+  navigation.classList.toggle('active')
+})
